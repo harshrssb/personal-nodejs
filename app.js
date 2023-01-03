c49166df29d285f03c0bc4ef1e8dc9eaff4e396b
harshrssb <116018649+harshrssb@users.noreply.github.com>
2023-01-03 15:28:32 +0530

    first coomit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+
+const url = 'mongodb://127.0.0.1/personal';
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+
+app.get('/', (req, res) => {
+    res.render('../mainPage.ejs')
+})
+
+
+
+app.listen(8080, () => {
+    console.log('8080')
+})
