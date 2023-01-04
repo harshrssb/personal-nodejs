3f6681d737800ec8c7f7cee31c1d3f997abf519a
harshrssb <116018649+harshrssb@users.noreply.github.com>
2023-01-04 14:56:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
 
 const url = 'mongodb://127.0.0.1/personal';
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
-
 
 
 app.get('/', (req, res) => {
@@ -15,6 +16,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(8080, () => {
-    console.log('8080')
+app.listen(port, () => {
+    console.log('3000')
 })
